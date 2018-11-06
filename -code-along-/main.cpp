e2462706c4523642a28c239709a2b41f1d977f94
ddj777 <ddj777@users.noreply.github.com>
2018-11-06 19:26:47 +0100

    local -ca- paste

--- -code-along-/main.cpp
+++ -code-along-/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+//- stdafx.h <=> copy from local ..\-code-along-\
+#include <string>
+#include <array>
+#include <memory>
+
+using namespace std;
+
+#include<intrin.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
