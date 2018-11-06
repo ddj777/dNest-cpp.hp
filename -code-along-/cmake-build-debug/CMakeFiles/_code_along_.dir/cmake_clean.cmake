976c7f5002a6216b481e1bd27ee8fa78fb9ea84f
ddj777 <ddj777@users.noreply.github.com>
2018-11-06 19:02:10 +0100

    Add files via upload
    git repo created here full, local cLion w/o cmake-build-debug dir

---
+++ -code-along-/cmake-build-debug/CMakeFiles/_code_along_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_code_along_.dir/main.cpp.obj"
+  "_code_along_.pdb"
+  "_code_along_.exe"
+  "_code_along_.exe.manifest"
+  "lib_code_along_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_code_along_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ -code-along-/cmake-build-debug/CMakeFiles/_code_along_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for _code_along_.
+# This may be replaced when dependencies are built.
